c93654908771c4805c3019f245dd0dccc3932cde
Deven <devenkapadia1@gmail.com>
2023-11-06 23:00:40 +0530

    Updates

---
+++ precache-manifest.8b178b90326ac1af5ae5489ae8e62005.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c8316a5b1d39f4756460e04d67e2ac",
+    "url": "/portfolio_deven/index.html"
+  },
+  {
+    "revision": "9795207b826711f76c6f",
+    "url": "/portfolio_deven/static/css/main.c8e595ac.chunk.css"
+  },
+  {
+    "revision": "e2acb7668ecf8455aac2",
+    "url": "/portfolio_deven/static/js/0.820cdce1.chunk.js"
+  },
+  {
+    "revision": "407f307c6ff4d6d1b372",
+    "url": "/portfolio_deven/static/js/10.5f0afac8.chunk.js"
+  },
+  {
+    "revision": "30e58856259d8e77a7e4",
+    "url": "/portfolio_deven/static/js/11.1eefcb04.chunk.js"
+  },
+  {
+    "revision": "3ca6c5283e9e0d3e9642",
+    "url": "/portfolio_deven/static/js/12.2e93ee6c.chunk.js"
+  },
+  {
+    "revision": "e9b286f5ca4a9707bad0",
+    "url": "/portfolio_deven/static/js/3.f497bbb3.chunk.js"
+  },
+  {
+    "revision": "d0ef5eba642f0edd857146cd0c823fd9",
+    "url": "/portfolio_deven/static/js/3.f497bbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0074ec132ac0dfe37a",
+    "url": "/portfolio_deven/static/js/4.6a990906.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio_deven/static/js/4.6a990906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb98be9b637ee7ddc09",
+    "url": "/portfolio_deven/static/js/5.442c21be.chunk.js"
+  },
+  {
+    "revision": "05e0194a71586cbf7494",
+    "url": "/portfolio_deven/static/js/6.45eec32f.chunk.js"
+  },
+  {
+    "revision": "8a7f91604701f2c06d72",
+    "url": "/portfolio_deven/static/js/7.f6aaa38b.chunk.js"
+  },
+  {
+    "revision": "1d06dcfd12c88d727dfe",
+    "url": "/portfolio_deven/static/js/8.d3176550.chunk.js"
+  },
+  {
+    "revision": "6ffa344fda10106592e9",
+    "url": "/portfolio_deven/static/js/9.63db3090.chunk.js"
+  },
+  {
+    "revision": "9795207b826711f76c6f",
+    "url": "/portfolio_deven/static/js/main.5b70ff4a.chunk.js"
+  },
+  {
+    "revision": "aab95711530507189f3b",
+    "url": "/portfolio_deven/static/js/runtime-main.e859e3b5.js"
+  },
+  {
+    "revision": "b842800d55ff534ed508d28babc38b81",
+    "url": "/portfolio_deven/static/media/agr.b842800d.jpg"
+  },
+  {
+    "revision": "c6a1f1328bae61e9b00f8f345a2e69a6",
+    "url": "/portfolio_deven/static/media/cldc.c6a1f132.jpg"
+  },
+  {
+    "revision": "a4ca2730f622d6cda664939288d6ec6b",
+    "url": "/portfolio_deven/static/media/gym.a4ca2730.jpg"
+  },
+  {
+    "revision": "cdc91ffe34f1f09d8663337ef32317e0",
+    "url": "/portfolio_deven/static/media/imdb.cdc91ffe.jpg"
+  },
+  {
+    "revision": "1593fd36a6c085b6fb354442f5eabec8",
+    "url": "/portfolio_deven/static/media/news.1593fd36.jpg"
+  },
+  {
+    "revision": "edb3b85e865b326b960a62c72c7dbb93",
+    "url": "/portfolio_deven/static/media/notes.edb3b85e.jpg"
+  },
+  {
+    "revision": "35ed372a97d31a8ad85bbb374827bf98",
+    "url": "/portfolio_deven/static/media/profile.35ed372a.jpg"
+  },
+  {
+    "revision": "aa0c80ca7448a2443d7b8ca8298afebd",
+    "url": "/portfolio_deven/static/media/util.aa0c80ca.jpg"
+  }
+]);
